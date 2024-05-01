f633a18fa00473f68ad5562b4573fd75c11a200f
musty4real2 <musty4real2@gmail.com>
2024-05-01 21:55:47 +0100

    changes to tested examples

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import Add from "./components/Add";
-import Greetings from "./components/Greetings";
+// import Add from "./components/Add";
+// import Greetings from "./components/Greetings";
+
+const myName='Mustapha WebDev';
+const multiply =(a,b)=>a*b;
+const specialClass='simple-class';
+
 function App() {
   return (
-    <section>
-      <Greetings/>
-      <Add />
-    </section>
+    <>
+    <h1>Welcome, I am {myName}</h1>
+    <p>2*2 = {2*2}</p>
+    <p>My Friends List {['Suleiman','Attahiru','Alpha Tee','Ahmed ']}</p>
+    <p>2*2= {multiply(2,2)}</p>
+    <p className={specialClass}>Testing class name assignment </p>
+    </>
     
   );
 }
