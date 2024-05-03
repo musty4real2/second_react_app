ad9df2a680e6f514f87cf044cc23100173258b87
musty4real2 <musty4real2@gmail.com>
2024-05-03 22:36:49 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ const Cart = () =>{
 
   return <>
 <h1>Cart</h1>
+{items.length > 0 && 
+  <h2>You have {items.length} items in your cart</h2>
+}
   <ul>
     <h4>Products</h4>
     {items.map((item => (
