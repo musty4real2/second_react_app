b610815b2600a4213d9923f97494e73647473400
musty4real2 <musty4real2@gmail.com>
2024-05-05 18:45:51 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
-import "./index.css";
-import { FaCartArrowDown } from "react-icons/fa";
-const Cart = () =>{
-  const items=['Wireless','Power Bank',' New SSD','Hoodies'];
-  const dStyle= {
-    color:"white",
-    backgroundColor:"crimson",
-    padding:"6rem"
+const Button = () =>{
+  const handleClick= () =>{
+    console.log("Button Clicked");
   }
-
-  return <>
-    <h1 className="underline decoration-pink-500">
-      Hello world!
-    </h1>
-<h1>Cart</h1>
-{/* {items.length > 0 &&  */}
-  {/* <h2 style={{color:"white",backgroundColor:"teal",padding:"2rem"}}>You have {items.length} items in your cart</h2>
-
-    <h2 style={{color:dStyle.color,backgroundColor:dStyle.backgroundColor,padding:dStyle.padding}}>You have {items.length} items in your cart</h2>}
-   */}
-
-{items.length > 0 && <h2>You have {items.length} items in your cart<FaCartArrowDown /></h2>}
-
-  <ul>
-    <h4>Products</h4>
-    {items.map((item => (
-      <li key={Math.random()}>{item}</li>
-    )))}
-  </ul>  
-  </> 
-  }
+  return <button onClick={handleClick}>Click</button>
+}
 function App() {
   return (
     <>
-    <Cart />  
+    <Button />
+
     </>
   );
 }
