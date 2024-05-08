77b7fe7e84358b74dcc138af98dae228203605e8
musty4real2 <musty4real2@gmail.com>
2024-05-08 17:21:00 +0100

    counter useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
 const Counter =() =>{
+  const [count, setCount] =useState(0);
+
+  const increase=() => {
+    setCount(count + 1);
+  }
+  const decrease=() => {
+    setCount(count - 1);
+  }
+
   return <>
-  <h1>0</h1>
-  <button>+</button>
-  <button>-</button>
+  <h1>{count}</h1>
+  <button onClick={increase}>+</button>
+  <button onClick={decrease}>-</button>
   </>
 }
 function App() {
