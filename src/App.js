e6eef4e820ae75033930d4bc1ab2be05e84b6274
musty4real2 <musty4real2@gmail.com>
2024-05-09 22:46:10 +0100

    filter f=>f !==

--- src/App.js
+++ src/App.js
@@ -11,11 +11,23 @@ import { useState } from "react";
 
 function App() {
   const [username, setUsername] =useState(["Alex","Iwobi"]);
+
+  const addOne= () =>{
+    setUsername([...username,"Musty"]);
+  }
+  const removeOne= () =>{
+    setUsername(username.filter(f => f !=="Alex"));
+  }
+  const updateOne= () =>{}
   return (
     <>
     {username.map((f) => (
       <li key={Math.random()}>{f}</li>
     ))}
+
+    <button onClick={addOne}> Add One</button>
+    <button onClick={removeOne}> Remove One</button>
+    {/* <button onClick={updateOne}> Update One</button> */}
     </>
   );
 }
