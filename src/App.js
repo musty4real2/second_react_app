9941c2a323a8115e302a87cdf90cb7d25c431942
musty4real2 <musty4real2@gmail.com>
2024-05-09 22:31:36 +0100

    useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import { useState } from "react";
-const Counter =() =>{
-  const [count, setCount] =useState(0);
+// const Counter =() =>{
+//   const [count, setCount] =useState(0);
 
-  const increase=() => {
-    setCount(count + 1);
-  }
-  const decrease=() => {
-    setCount(count - 1);
-  }
+//   const increase=() => {
+//     setCount(count + 1);
+//   }
+//   const decrease=() => {
+//     setCount(count - 1);
+//   }
 
-  return <>
-  <h1>{count}</h1>
-  <button onClick={increase}>+</button>
-  <button onClick={decrease}>-</button>
-  </>
-}
 function App() {
+  const [username, setUsername] =useState(["Alex","Iwobi"]);
   return (
     <>
-  <Counter />
+    {username.map((f) => (
+      <li key={Math.random()}>{f}</li>
+    ))}
     </>
   );
 }
