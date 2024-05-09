3c83b0dacfdd89cbd7dfe96cf406c76b22ab9527
musty4real2 <musty4real2@gmail.com>
2024-05-09 23:16:21 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
 import { useState } from "react";
-// const Counter =() =>{
-//   const [count, setCount] =useState(0);
-
-//   const increase=() => {
-//     setCount(count + 1);
-//   }
-//   const decrease=() => {
-//     setCount(count - 1);
-//   }
-
 function App() {
-  const [username, setUsername] =useState(["Alex","Iwobi"]);
+  const [movie, setMovie] = useState({ title: "Equalizer", year: 2024, rating: 7 });
 
-  const addOne= () =>{
-    setUsername([...username,"Mudassir"]);
-  }
-  const removeOne= () =>{
-    setUsername(username.filter((f) => f !=="Musty"));
-  }
-  const updateOne= () =>{
-    setUsername(username.map((f)=> (f === 'Alex' ? 'Alex Smith': f)));
-  }
+  const changeRating = () => {
+    const copyMovie = {
+      ...movie,
+      rating: 6,
+    }
+
+    setMovie(copyMovie);
+  };
+  
   return (
     <>
-    {username.map((f) => (
-      <li key={Math.random()}>{f}</li>
-    ))}
-
-    <button onClick={addOne}> Add One</button>
-    <button onClick={removeOne}> Remove One</button>
-    {/* <button onClick={updateOne}> Update One</button> */}
+      <h1>{movie.title}</h1>
+      <p>{movie.year}</p> {/* Display the year instead of title */}
+      <button onClick={changeRating}>Change Ratings</button>
     </>
   );
 }
