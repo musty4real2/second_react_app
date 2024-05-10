cfb055f7806864081afe5686c3d74f2a27de7307
musty4real2 <musty4real2@gmail.com>
2024-05-10 12:57:01 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -3,18 +3,16 @@ function App() {
   const [movie, setMovie] = useState({ title: "Equalizer", year: 2024, rating: 7 });
 
   const changeRating = () => {
-    const copyMovie = {
-      ...movie,
-      rating: 6,
-    }
+    // const copyMovie = {...movie,rating: 6,}
 
-    setMovie(copyMovie);
+    setMovie({...movie,rating: 6,});
   };
   
   return (
     <>
       <h1>{movie.title}</h1>
       <p>{movie.year}</p> {/* Display the year instead of title */}
+      <p>{movie.rating}</p>
       <button onClick={changeRating}>Change Ratings</button>
     </>
   );
