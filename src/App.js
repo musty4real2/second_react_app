ad504b39301bd4dac3bd7b09599c4451abc3e7ae
musty4real2 <musty4real2@gmail.com>
2024-05-11 20:59:51 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import { useState } from "react";
 function App() {
-  const [movie, setMovie] = useState({ title: "Equalizer", year: 2024, rating: 7 });
+  const [movies, setMovies] = useState(
+    { id: 1, title: "Equalizer", year: 2024, rating: 7 },
+    { id: 2, title: "Spiderman", year: 2009, rating: 2 },
+    { id: 3, title: "Immortals", year: 1999, rating: 4 },
+  );
 
-  const changeRating = () => {
-    // const copyMovie = {...movie,rating: 6,}
+  // const changeRating = () => {
+  //   // const copyMovie = {...movie,rating: 6,}
 
-    setMovie({...movie,rating: 6,});
-  };
+  //   setMovie({...movie,rating: 6,});
+  // };
   
   return (
     <>
-      <h1>{movie.title}</h1>
-      <p>{movie.year}</p> {/* Display the year instead of title */}
-      <p>{movie.rating}</p>
-      <button onClick={changeRating}>Change Ratings</button>
+      {movies.map(movie => (
+        <li>{movie.title}</li>
+      ))}
+    
+      {/* <h1>{movie.title}</h1> */}
+      {/* <p>{movie.year}</p> Display the year instead of title */}
+      {/* <p>{movie.rating}</p> */}
+      {/* <button onClick={changeRating}>Change Ratings</button> */}
     </>
   );
 }
